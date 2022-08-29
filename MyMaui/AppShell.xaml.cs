de7673e38a1c755c7fcd52bd79c5520653c1d99f
jaejoon.han <jaejoon.han@crevisse.com>
2022-08-30 01:17:01 +0900

    Add project files.

---
+++ MyMaui/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MyMaui;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MyMaui/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace MyMaui;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    async void OnCameraClickedAsync(object sender, EventArgs e)
+    {
+        if (MediaPicker.Default.IsCaptureSupported)
+        {
+            FileResult photo = await MediaPicker.Default.CapturePhotoAsync();
+
+            if (photo != null)
+            {
+                // save the file into local storage
+                string targetFile = Path.Combine(FileSystem.Current.AppDataDirectory, photo.FileName);
+
+                using Stream sourceStream = await photo.OpenReadAsync();
+                using FileStream outputStream = File.OpenWrite(targetFile);
+
+                await sourceStream.CopyToAsync(outputStream);
+            }
+        }
+    }
+}
